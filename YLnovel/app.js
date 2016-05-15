c2f96126c19eefc7708d8180c90ac1025e15705f
unknown <ronnieyuyiwei@163.com>
2016-05-15 16:26:18 +0800

    我的主页

--- YLnovel/app.js
+++ YLnovel/app.js
@@ -5,9 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+//var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -22,8 +20,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+app.get('/', function (req, res) {
+  res.render('index',{})
+});
+app.get('/mypage', function (req, res) {
+  res.render('mypage', {}
+  )
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
